6034f158b0e6c9cb44e4c6712d5ec34917fcba63
atesagaoglu <atesagaoglu@outlook.com>
2025-08-05 23:06:30 +0300

    feat: new flags - "--no-deps" prevents dependencies to be called - "--list-deps" list dependencies of a defined environment - "--list/-l" lists all defined environments

--- denv.py
+++ denv.py
@@ -13,11 +13,19 @@ cmds = []
 # default file, can be set with --config
 config_file = os.path.expandvars("$XDG_CONFIG_HOME/denv.yaml")
 
+args = None
+
 
 def process(key):
 
     with open(config_file, "r") as file:
         obj = yaml.load(file, Loader=yaml.SafeLoader)
+
+        # MAYBE: Do this outside and remember keys
+        if args.list:
+            for k in obj.keys():
+                print(f"echo - {k}")
+            exit()
 
         try:
             selected = obj[key]
@@ -25,22 +33,30 @@ def process(key):
             print(f"Environment {key} not found!")
             exit()
 
-        if "path" in selected:
-            path = selected["path"]
-            paths.extend(path)
+        # Do not set anything if list is set
+        if not args.list_deps:
 
-        if "var" in selected:
-            var = selected["var"]
-            vars.update(var)
+            if "path" in selected:
+                path = selected["path"]
+                paths.extend(path)
 
-        if "cmd" in selected:
-            cmd = selected["cmd"]
-            cmds.extend(cmd)
+            if "var" in selected:
+                var = selected["var"]
+                vars.update(var)
+
+            if "cmd" in selected:
+                cmd = selected["cmd"]
+                cmds.extend(cmd)
+        else:
+            print(f'echo "Dependencies:"')
 
         # Recursively call dependencies
-        if "dep" in selected:
+        if (not args.no_deps) and "dep" in selected:
             dep = selected["dep"]
             for d in dep:
+                if args.list_deps:
+                    print(f"echo - {d}")
+                    continue
                 process(d)
 
 
@@ -54,7 +70,20 @@ if __name__ == "__main__":
     )
     parser.error = lambda msg: print(msg) or parser.print_help() or exit(1)
 
-    parser.add_argument("key", help="Dev environement to setup from the file.")
+    parser.add_argument(
+        "key", help="Dev environement to setup from the file.", nargs="?", default=None
+    )
+    parser.add_argument(
+        "--no-deps",
+        help="Setup an environement without setting up it's dependencies",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--list", "-l", help="Lists defined environments.", action="store_true"
+    )
+    parser.add_argument(
+        "--list-deps", help="Lists dependencies of an environment.", action="store_true"
+    )
     parser.add_argument(
         "-c", "--config", help="Specify a config file to use.", type=pathlib.Path
     )
@@ -64,12 +93,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # key can only be None when list is set
+    if not args.list and not args.key:
+        parser.print_help()
+
     if args.config and os.path.isfile(args.config):
         config_file = args.config
 
     process(args.key)
 
-    print(f'export PATH={":".join(paths)}:$PATH')
+    if not args.list_deps:
+        print(f'export PATH={":".join(paths)}:$PATH')
 
     for key, value in vars.items():
         print(f'export {key}="{value}"')
